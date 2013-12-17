8e5c43a662e8b3aa4864c454154e914bfe5f0b69
jeffhoffman1 <jeffhoffman1@gmail.com>
2013-12-17 12:41:18 -0700

    Fixed up server, loaded it to amazon too

--- StoryHub/server/server.js
+++ StoryHub/server/server.js
@@ -1,38 +1,97 @@
 var express = require('express');
 var app = express();
 
-var MONGOHQ_URL="mongodb://jeffhoffman1:a1234567@dharma.mongohq.com:10061/storyhub"
-var test, hero, mongoose, _;
+var MONGOHQ_URL="mongodb://jeffhoffman1:a1234567@dharma.mongohq.com:10061/storyhub";
+var test, mongoose, _;
 
 var passport = require('passport');
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login' }));
- 
+app.post('/login', passport.authenticate('local', 
+	{ successRedirect: '/',
+      failureRedirect: '/login' }));
+	  
 mongoose = require("mongoose");
- 
+var Schema = mongoose.Schema;
+
+var StorySchema = new Schema({
+	StoryID	: String,
+	UserID	: String,
+	Genre	: String,
+	Title	: String,
+	Image	: String,
+	Options	: {Something : String},
+	Upvotes : Number,
+	Downvotes : Number	
+});
+
+var RevisionSchema = new Schema({
+	RevisionID : String,
+	UserID	: String,
+	StoryID : String,
+	Type	: String,
+	RevisionBranchID : String,
+	Text	: String,
+	Upvotes	: Number,
+	Downvotes : Number
+});
+
+var RevisionBranchSchema = new Schema({
+	RevisionBranchID : String,
+	RevisionIDs	: String,
+	UserID		: String,
+	OriginalRevisionID	: String,
+	VoteWeight	: String
+});
+
+var VoteSchema = new Schema({
+	VoteID	: String,
+	UserID	: String,
+	RevisionID	: String,
+	Vote	: String
+});
+
+var RatingSchema = new Schema({
+	StoryID	: String,
+	UuserID	: String,
+	Stars	: Number
+});
+
 _ = require("underscore");
+var mongodb = require('mongodb');
  
-mongoose.connect(MONGOHQ_URL);
- console.log("CONNECTED!");
-test = mongoose.model('test', { name: "string" });
- console.log("MODELED!");
-hero = new test({
-  name: "Superman"
+ //mongo dharma.mongohq.com:10061/storyhub -u jeffhoffman1 -p a1234567
+ 
+mongoose.connect(MONGOHQ_URL, function(err){
+	throw err;}
+);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  // yay!
 });
- console.log("HERO!",hero);
-hero.save(function(err) {
-console.log("SAVE?");
-  if (err) {
-    return console.log("fail");
-  } else {
-    return console.log("win");
-  }
-});
- console.log("SAVED!");
-test.find({}, function(err, documents) {
-console.log("DOCS!",documents);
-  return console.log(documents[0]);
-});
-console.log("FOUND!");
+
+function save(obj,name){
+	var schema = name + "Schema";
+	var model = mongoose.model(name,schema);
+	var object = new model(obj);
+	object.save();
+	return object;
+}
+
+function findwhere(name,where){
+	var schema = name + "Schema";
+	var model = mongoose.model(name,schema);
+	return model.find(where);
+}
+
+function find(name){
+	var schema = name + "Schema";
+	var model = mongoose.model(name,schema);
+	return model.find(function(err,docs){
+		if(err){
+			return err;
+		}
+		return docs;
+	});
+}
